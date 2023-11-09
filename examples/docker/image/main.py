433d6d26e3559f431f3a3cef7eb46add83324b05
vetrovsv <eridani_alpha@hotmail.com>
2023-11-09 23:28:05 +0900

    + docker example and doc

---
+++ examples/docker/image/main.py
@@ -0,0 +1,39 @@
+# Веб-сервер, предоставляет интерфейс для рисования цифр для последующего распознавания случайным лесом
+
+
+import numpy as np
+import gradio as gr
+from joblib import load, dump
+
+# файл обученной модели
+MODEL_FILE = 'forest.joblib.lzma'
+
+
+
+def predict(img: np.ndarray):
+    """Обработчик отправки формы; Распознаёт по картинке число, выдаёт top1 лейбл класса;
+    img — np.ndarray[28, 28]
+    Model — модель с методом predict"""
+    global Model
+    # predict принимает список из объектов; возвращает тоже список из ответов
+    return Model.predict( img.reshape(1,-1) ) 
+    # img.reshape(1,-1)  -> shape[ 1, auto ]; здесь будет auto = 784
+
+
+
+# обученная модель sklearn RandomForestClassifier сохранена так
+# dump(forest, 'forest.joblib', compress=9) 
+# загрузка модели; важно чтобы версии joblib для сохранения и загрузки совпадали
+Model = load(MODEL_FILE)
+
+
+# todo: изменить под новую версию gradio, где всё поменяли...
+# панель для рисования; image_mode = L - ч/б изображение
+canvas = gr.inputs.Image(shape=(28,28), image_mode="L", invert_colors=True, 
+                         source="canvas", tool="editor", type="numpy")
+
+
+
+demo = gr.Interface(fn=predict, inputs=canvas, outputs="text")
+demo.launch(show_api=False, server_name="0.0.0.0", server_port=7860)   
+# в основной ос: http://localhost:7860
