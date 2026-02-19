857cffe6f90b1b49dd195f38dc0deba1588d74f6
VetrovSV <eridani_alpha@hotmail.com>
2026-02-19 13:26:57 +0900

    upd: parser example

--- DataProcessing/example_page/get_elements.py
+++ DataProcessing/example_page/get_elements.py
@@ -6,20 +6,43 @@ url = 'example.html'
 with open(url, "r", encoding="utf-8") as f:
     html = f.read()
 
+# soup_example - экземпляр распашеной страницы
 soup_example = BeautifulSoup(html, 'html.parser')
 
 print(f"Заголовок страницы {soup_example.title}")
+# см. также другие поля классов, представляющих страницу и элементы DOM
 
 # 1. Запрос по тегу: выбрать все sections
-divs: ResultSet = soup_example.select('section')
+sections: ResultSet = soup_example.find_all('section')
 
-print(f"selected sections: {len(divs)}")
-print(f"first section: {divs[0].text}")
-# 1. Запрос по тегу: выбрать все div
-divs = soup_example.select('div')
-print(f"selected divs: {len(divs)}")
+print(f"selected sections: {len(sections)}")
+print(f"first section: {sections[0].text}")
+# 1. Запрос по тегу: выбрать все элементы с тегом span
+spans = soup_example.select('span')
+print(f"selected spans: {len(spans)}")
 
 
 
 
 # 2. Выбор по атрибутам
+spans_price = soup_example.find_all('span', class_ = 'price')
+print(f"selected divs: {len(spans_price)}")
+
+
+# 3. Выбор по иерархии
+items = soup_example.find("section", class_="products").find_all("li", class_="item")
+print(f"selected items: {len(items)}")
+print(f"first item: {items[0].text}")
+
+
+
+# 4. Выборе по CSS селектору: выбрать все li c CSS классом item,
+# которые находятся внутри ul c CSS классом items
+# Шпаргалка: https://htmlacademy.ru/blog/css/selectors
+items =  soup_example.select("ul.items > li.item")
+print(f"selected items: {len(items)}")
+print(f"first item: {items[0].text}")
+
+
+
+# 5.
---
+++ DataProcessing/examples/c1news_scraper/config.py
@@ -0,0 +1,12 @@
+SITE_URL = "https://www.cnews.ru/"
+DATA_DIR = "data"
+
+
+SQL_FILENAME = "database.sqlite"
+PAGES_URL_TABLE = "pages_urls"
+
+PAGES_HTML_TABLE = "pages_content"
+STATE_TO_PROCESS = "new"
+
+# задержка в секундах между скачиванием отдельных страниц
+DELAY = 3
